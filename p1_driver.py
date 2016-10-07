e95e631d258cf06bc6bb2cfd92cf197755810f23
friketrike <friketrike@gmail.com>
2016-10-07 10:40:44 -0400

    something broke, methinks

---
+++ p1_driver.py
@@ -0,0 +1,38 @@
+
+def test():
+    import search_agent
+    import search_algorithms
+
+    class Node:
+        children = []
+        parent = None
+        value = None
+
+    root = Node()
+    root.value = 1
+
+    def generate_children(node):
+        node1 = Node()
+        node1.value = node.value+1
+        node2 = Node()
+        node2.value = node.value+2
+        node.children = [node1, node2]
+
+    def goal_test(node):
+        return node.value > 9
+
+    open_list = [root]
+    closed_list = []
+
+    path = search_agent.find_goal(open_list, closed_list, 
+                                  search_algorithms.dfs, 
+                                  goal_test, generate_children)
+    while path:
+        n = path.pop(0)
+        print 'value of this node is: '
+        print n.value
+        print ' and the parent\'s node is: '
+        if n.parent:
+            print n.parent.value
+        else:
+            print ' no parent...'
--- search_agent.py
+++ search_agent.py
@@ -9,23 +9,29 @@
 # import problem_node.py as pn
 # impora serach_algorithms.py as sa
 
+def set_path_to_goal(node):
+    path_to_goal = []
+    while (node.parent):
+        path_to_goal.insert(0, node)
+        node = node.parent
+        # TODO do we really want to delete the upstream reference?
+        # del(path_to_goal[0].parent)
+
+    # don't forget to include the start state!    
+    path_to_goal.insert(0, node)
+    return path_to_goal
+
 def find_goal(open_list, closed_list, search_algorithm, goal_test, generate_children):
     # TODO check that search_algorithm(), goal_test() and generate_children()
     # exist, otherwise give a usage warning
-    path_to_goal = []
     while(open_list):
         curr_node = open_list.pop(0)
         # We found it, return a list with the path
         if goal_test(curr_node):
-            while (curr_node.parent):
-                path_to_goal.insert(0, curr_node)
-                curr_node = curr_node.parent
-                # TODO do we really want to delete the upstream reference?
-                delete(path_to_goal[0].parent)
-            return path_to_goal
+            return set_path_to_goal(curr_node)
         else:
             closed_list.append(curr_node)
-            curr_node = search_algorithm(curr_node, open_list, closed_list, generate_children)
+            search_algorithm(curr_node, open_list, closed_list, generate_children)
 
     return None
 
